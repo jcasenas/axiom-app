5f03729037cb7d348e27c18c82d6ec63a6b396af
jcasenas <j.casenas.557455@umindanao.edu.ph>
2026-04-28 17:26:19 +0800

    fix: trust proxies for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        'role'  => \App\Http\Middleware\RoleMiddleware::class,
-    ]);
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'role'  => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
